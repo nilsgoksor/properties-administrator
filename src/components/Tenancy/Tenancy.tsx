df9b41bb51d2f8eb0963abf672b2b8edb5e5e2bb
Nils <nils.goksor@gmail.com>
2022-02-22 14:26:52 +0100

    fix: styling of tenancy card

--- src/components/Tenancy/Tenancy.tsx
+++ src/components/Tenancy/Tenancy.tsx
@@ -1,24 +1,56 @@
-import { Avatar, Card, CardContent, CardHeader } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Collapse,
+  IconButton,
+} from "@mui/material";
 import { TenancyI } from "../../model";
+import InfoIcon from "@mui/icons-material/InfoRounded";
+import { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 
 interface TenancyProps {
   tenancy: TenancyI;
 }
 
 export const Tenancy = ({ tenancy }: TenancyProps) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const hasUtilities = tenancy.utilities[0].length > 0;
+
   return (
     <Card>
       <CardHeader
         avatar={<Avatar>{tenancy.landlord.image}</Avatar>}
         title={tenancy.address.street}
+        action={
+          <IconButton onClick={() => setExpanded(!expanded)}>
+            <InfoIcon />
+          </IconButton>
+        }
         subheader={`${tenancy.address.city} - ${tenancy.address.country}`}
       />
-      <CardContent>
-        {tenancy.utilities.map((u) => (
-          <p>{u}</p>
-        ))}
-        <div>{tenancy.rooms}</div>
-      </CardContent>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Stack direction="row" spacing={1}>
+            <Chip label={`${tenancy.rooms} rooms`} />
+            {tenancy.size && <Chip label={`${tenancy.size} m2`} />}
+          </Stack>
+          {hasUtilities && (
+            <>
+              <p>With the following utilities:</p>
+              <Stack direction="row" spacing={1}>
+                {tenancy.utilities.map((u) => (
+                  <Chip key={u} label={`${u}`} />
+                ))}
+              </Stack>
+            </>
+          )}
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
